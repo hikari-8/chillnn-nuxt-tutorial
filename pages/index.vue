ac91eada8bc193b7038694f5a49ef57d5158439e
hikari-8 <kanbarusuruga0127@gmail.com>
2022-09-13 13:33:46 +0900

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="app_container">
+    <div class="message">{{ message }}</div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from "nuxt-property-decorator";
+
+//@Componet~exportの部分全てで、vue-property-decorator式でクラスを定義している
+@Component({
+  //型を定義する場所
+})
+export default class TopPage extends Vue {
+  public message = "HELLO WORLD, welcome to STAGE2";
+}
+</script>
+
+<style lang="stylus" scoped>
+.app_container {
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  .message {
+    font-size: 40px;
+    text-slign: center;
+    font-weight: bold;
+  }
+}
+</style>
